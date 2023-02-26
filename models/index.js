103ca02aae213d49a64eb420e4f851dd4c6116d4
gorillaRouge <john.schoppmann@yahoo.com>
2023-02-26 16:33:06 -0700

    added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const User = require('./User');
-const Post = require('./Post');
+const Post = require('../models/Post');
 const Comment = require('./Comment');
 
 // Post belongs to User, defining foreign key and onDelete method
